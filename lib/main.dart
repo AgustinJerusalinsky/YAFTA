5abdbbcb3f7e37d9164b114ff5cbb8a894fb91ca
camilaborinsky <cborinsky@itba.edu.ar>
2023-03-22 17:27:53 +0000

    style(theme): add light and dark color schemes and light and dark themes to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yafta/theme/theme_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
